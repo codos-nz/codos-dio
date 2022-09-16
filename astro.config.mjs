2adb8235f21b7c238538409ecd6d04a25351537c
Adam Du <dulinnan@gmail.com>
2022-09-17 08:18:56 +1200

    feat: setup pnpm and Splash img

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://dio.codos.co.nz",
   integrations: [tailwind()],
   vite: {
     ssr: {
       external: ["@11ty/eleventy-img", "svgo"],
     },
   },
+  output: "server",
+  adapter: cloudflare({ mode: "directory" }),
 });
