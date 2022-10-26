e2cd120d58db17a51d37400c18ea03395265fd20
udmada <github@adamdu.nz>
2022-10-27 05:03:22 +1300

    feat: use Vercel SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,22 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://dio.codos.co.nz",
-  integrations: [react(), tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  integrations: [
+    react(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
   vite: {
     ssr: {
       external: ["svgo"],
     },
   },
+  adapter: vercel(),
 });
