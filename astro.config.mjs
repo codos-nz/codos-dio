f8036fcf34385ce5bfa34c81862ea13cb486e9a1
udmada <github@adamdu.nz>
2022-10-30 23:37:03 +1300

    fix: turn off svg compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,18 +27,7 @@ export default defineConfig({
       html: false,
       img: true,
       js: false,
-      svg: {
-        multipass: true, // boolean. false by default
-        datauri: "enc", // 'base64' (default), 'enc' or 'unenc'.
-        js2svg: {
-          indent: 2, // string with spaces or number of spaces. 4 by default
-          pretty: true, // boolean, false by default
-        },
-        plugins: [
-          // set of built-in plugins enabled by default
-          "preset-default",
-        ],
-      },
+      svg: false,
     }),
   ],
 });
