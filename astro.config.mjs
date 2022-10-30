222245a77443b2484819264b719ee24457eb143f
udmada <github@adamdu.nz>
2022-10-30 23:25:00 +1300

    fix: use icon for svg wordmark

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,18 @@ export default defineConfig({
       html: false,
       img: true,
       js: false,
-      svg: false,
+      svg: {
+        multipass: true, // boolean. false by default
+        datauri: "enc", // 'base64' (default), 'enc' or 'unenc'.
+        js2svg: {
+          indent: 2, // string with spaces or number of spaces. 4 by default
+          pretty: true, // boolean, false by default
+        },
+        plugins: [
+          // set of built-in plugins enabled by default
+          "preset-default",
+        ],
+      },
     }),
-  ]
+  ],
 });
