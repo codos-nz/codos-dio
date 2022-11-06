1cb3dd62112e42ebfac88d873977062aa6b37aa6
udmada <github@adamdu.nz>
2022-11-06 16:04:56 +1300

    feat: add Shrikhand font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: [...defaultTheme.fontFamily.sans],
+        sans: [ ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: "var(--color-primary)",
