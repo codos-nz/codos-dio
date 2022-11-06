54cc2b7fd2efccdca4144516329243b5a56f75c3
udmada <github@adamdu.nz>
2022-11-07 09:43:07 +1300

    feat: optimise SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import image from "@astrojs/image";
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://codos-dio.vercel.app",
@@ -13,6 +16,7 @@ export default defineConfig({
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    sitemap(),robotsTxt(),
     compress({
       css: false,
       html: false,
