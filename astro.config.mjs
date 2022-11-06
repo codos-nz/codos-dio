9514fb71c0d6dbace4b066c6f55cda1b838deb4e
udmada <github@adamdu.nz>
2022-11-06 16:09:37 +1300

    feat: vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
-
+import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dio.codos.co.nz",
+  site: "https://codos-dio.vercel.app",
+  output: "server",
+  adapter: vercel(),
   integrations: [
     tailwind(),
     image({
