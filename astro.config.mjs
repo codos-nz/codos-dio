9918612c4b020c0d49daa41f61774c514b51b1be
udmada <git@adamdu.nz>
2022-12-12 10:30:58 +1300

    chore(ci): update site definition

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://codos-dio.vercel.app",
+  site: "https://dragitout.co.nz",
   output: "server",
   adapter: vercel(),
   integrations: [
@@ -18,10 +18,10 @@ export default defineConfig({
     }),
     sitemap({
       customPages: [
-        "https://codos-dio.vercel.app/",
-        "https://codos-dio.vercel.app/events",
-        "https://codos-dio.vercel.app/about-us",
-        "https://codos-dio.vercel.app/contact",
+        "https://dragitout.co.nz/",
+        "https://dragitout.co.nz/events",
+        "https://dragitout.co.nz/about-us",
+        "https://dragitout.co.nz/contact",
       ],
     }),
     robotsTxt(),
