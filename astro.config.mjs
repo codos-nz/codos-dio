dd0b478e03307e1f2ba38fa8a658a3e4da411340
udmada <git@adamdu.nz>
2023-03-10 12:39:27 +1300

    feat: use www. base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dragitout.co.nz",
+  site: "https://www.dragitout.co.nz",
   output: "server",
   adapter: vercel(),
   integrations: [
@@ -18,10 +18,10 @@ export default defineConfig({
     }),
     sitemap({
       customPages: [
-        "https://dragitout.co.nz/",
-        "https://dragitout.co.nz/events",
-        "https://dragitout.co.nz/about-us",
-        "https://dragitout.co.nz/contact",
+        "https://www.dragitout.co.nz/",
+        "https://www.dragitout.co.nz/events",
+        "https://www.dragitout.co.nz/about-us",
+        "https://www.dragitout.co.nz/contact",
       ],
     }),
     robotsTxt(),
